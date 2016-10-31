c14cdc7296af300645dc8473dcf52710b9ef2276
Jim Ivanoff <jim@polarbeardesign.net>
2016-10-31 11:58:50 -0500

    more commenting out

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -41,33 +41,6 @@ namespace :deploy do
    task :stop do ; end
 
 
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
 
-  desc "symlink my db file"
-  task :symlink_db_file do
-    run "ln -s /home/#{user}/#{application}/shared/database.yml #{latest_release}/config/database.yml"
-  end
-
-  desc "symlink my db file again"
-  task :symlink_db_file_II do
-    run "ln -s /home/#{user}/#{application}/shared/database.yml /home/#{user}/#{application}/current/config/database.yml"
-  end
-
-  desc "symlink my env file"
-  task :symlink_env_file do
-    run "ln -s /home/#{user}/#{application}/shared/production.rb /home/#{user}/#{application}/current/config/environments/production.rb"
-  end
-
-  desc "symlink my htaccess file"
-  task :symlink_htaccess_file do
-    run "ln -s /home/#{user}/#{application}/shared/.htaccess /home/#{user}/#{application}/current/public/.htaccess"
-  end
-
-  desc "precompile other"
-  task :precompile_other do
-    run "cd /home/#{user}/#{application}/current && bundle exec rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile"
-  end
 
 end
