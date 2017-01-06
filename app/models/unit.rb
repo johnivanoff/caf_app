7e2dcfc474895563ccc4ddadc7ca0f7cf32f6409
Jim Ivanoff <jim@polarbeardesign.net>
2017-01-06 11:20:47 -0600

    updating aircraft format page

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,9 +3,9 @@ class Unit < ActiveRecord::Base
   has_many :social_links
   has_many :members, :through => :unit_assignments
   has_many :social_links, :dependent => :destroy
-  attr_accessible :address_1, :address_2, :author_username, :city, :description, :facebook, :museum, :postal_code, :primary_email, :primary_phone, :social_links_attributes, :state, :unit_name, :website, :unit_logo
+  attr_accessible :address_1, :address_2, :author_username, :city, :description, :facebook, :museum, :postal_code, :primary_email, :primary_phone, :state, :unit_name, :website, :unit_logo, :social_links_attributes
 
-  accepts_nested_attributes_for :social_links, :allow_destroy => true
+  accepts_nested_attributes_for :social_links
   
   has_paper_trail :class_name => 'UnitVersion',
      :meta => { :author_username => :user_name}
