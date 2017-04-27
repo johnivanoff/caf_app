549ad2ccfd1ee1cc37d114c51b7b4381c7c94e72
Jim Ivanoff <jim@polarbeardesign.net>
2017-04-27 10:32:47 -0500

    added toc to caf_documents

--- app/models/caf_document.rb
+++ app/models/caf_document.rb
@@ -1,6 +1,6 @@
 class CafDocument < ActiveRecord::Base
   belongs_to :document_category
-  attr_accessible :body, :current, :description, :document_category_id, :file_url, :image_url, :published_date, :title
+  attr_accessible :body, :current, :description, :document_category_id, :file_url, :image_url, :published_date, :title, :toc
 
   scope :category_ordered, joins(:document_category).merge(DocumentCategory.ordered)
   scope :ordered, order("caf_documents.title ASC")
