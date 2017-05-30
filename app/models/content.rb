4f7034eacdd882db7f9c054e8ca5d70b69107b57
Jim Ivanoff <jim@polarbeardesign.net>
2017-05-30 08:33:38 -0500

    adding param to slug for content

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,11 @@
 class Content < ActiveRecord::Base
   belongs_to :user
   attr_accessible :body, :published_at, :slug, :title, :user_id
+
+  validates_presence_of :slug
+
+  def to_param
+    "#{id}-#{slug}"
+  end
+  
 end
