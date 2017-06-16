2b4d72580637be9c68606bb39fea1e1344f5d726
Jim Ivanoff <jim@polarbeardesign.net>
2017-06-16 12:28:29 -0500

    added filter to hq positions

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,8 @@
 class Content < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :body, :published_at, :slug, :title, :user_id
+  attr_accessible :body, :content_category_id, :published_at, :slug, :title, :user_id
+
+  belongs_to :content_category
 
 #  validates_presence_of :slug
 
@@ -8,8 +10,21 @@ class Content < ActiveRecord::Base
 #    "#{id}-#{slug}"
 #  end
   
+  scope :pages, where("content_type_id = 1")
+  scope :categorize, order("contents.content_category_id ASC")
+  
   scope :news, where("content_type_id = 2")
   scope :tease, :limit => 4
   scope :reverse, order("contents.published_at DESC")
+
+
+# Content category ids
+#
+# 1 = About
+# 2 - Education
+# 3 - Members
+# 4 - Contact
+# 5 - Development
+# 6 - Membership
 
 end
--- app/models/hq_position.rb
+++ app/models/hq_position.rb
@@ -1,5 +1,6 @@
 class HqPosition < ActiveRecord::Base
   attr_accessible :description, :position_name
+  has_many :hq_position_assignments
   has_many :members, :through => :hq_position_assignments
 
   scope :alpha_order, order("position_name ASC")
