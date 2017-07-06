64afa65acfb1dcf85d1eb0751daf3019cf3403e3
Jim Ivanoff <jim@polarbeardesign.net>
2017-07-06 11:46:20 -0500

    updated root path

--- config/routes.rb
+++ config/routes.rb
@@ -200,7 +200,7 @@ resources :users do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'contents#home'
+   root :to => 'units'
 
   # See how all your routes lay out with "rake routes"
 
