6acc84876b7c39a40e58a32f75aeecece946c7dd
Jim Ivanoff <jim@polarbeardesign.net>
2017-07-18 18:37:36 -0500

    updates to units form

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -5,7 +5,10 @@ class Unit < ActiveRecord::Base
   #has_many :social_links
   has_many :members, :through => :unit_assignments
   has_many :social_links, :dependent => :destroy
-  attr_accessible :address_1, :address_2, :airport_name, :author_username, :city, :description, :facebook, :latitude, :longitude, :museum, :postal_code, :primary_email, :primary_phone, :state, :unit_name, :website, :unit_logo, :unit_type_id,:social_links_attributes
+  attr_accessible :address_1, :address_2, :airport_name, :author_username, :city, :description, 
+                  :facebook, :latitude, :longitude, :museum, :postal_code, :primary_email, 
+                  :primary_phone, :social_links_attributes, :state, :unit_name, :website, 
+                  :unit_logo, :unit_type_id
 
   accepts_nested_attributes_for :social_links, :allow_destroy => true
   
