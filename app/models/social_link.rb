d77d5a16b8a9249592a70a1be225351e1968aad1
Jim Ivanoff <jim@polarbeardesign.net>
2017-07-18 13:28:38 -0500

    updates to get remove social link to work

--- app/models/social_link.rb
+++ app/models/social_link.rb
@@ -1,5 +1,7 @@
 class SocialLink < ActiveRecord::Base
-  belongs_to :units
+  #belongs_to :units
+  #belongs_to :social_brand
+  belongs_to :unit
   belongs_to :social_brand
   attr_accessible :id, :brand_url, :social_brand_id, :unit_id, :created_at, :updated_at
 end
