38d3b01c96dd6d864fe861d5eb79b51c2778505b
Jim Ivanoff <jim@polarbeardesign.net>
2017-11-29 15:04:59 -0600

    added call to types

--- app/controllers/aircraft_types_controller.rb
+++ app/controllers/aircraft_types_controller.rb
@@ -14,6 +14,7 @@ class AircraftTypesController < ApplicationController
 
   def new
     @aircraft_type = AircraftType.new
+    @aircraft_classes = AircraftClass.find(:all)
     respond_with(@aircraft_type)
   end
 
