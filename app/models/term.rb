4697a1b08ccb25852ea721897258d8040b3107fd
Jim Ivanoff <jim@polarbeardesign.net>
2018-02-24 14:07:00 -0600

    added validation to term creation

--- app/models/term.rb
+++ app/models/term.rb
@@ -3,6 +3,9 @@ class Term < ActiveRecord::Base
   belongs_to :member
   attr_accessible :position_id, :member_id, :term_end, :term_start
 
+  validates :position_id, :member_id, :term_end, :term_start, :presence => true, :on => :create
+
+
   scope :ordered, order("terms.position_id ASC") 
   scope :current, lambda {
     where ("terms.term_start IS NOT NULL AND terms.term_start < ? AND terms.term_end > ? "), (Date.today), (Date.today)
