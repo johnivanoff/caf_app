4e0625b609c0a81c94773b642445c4304407eceb
Jim Ivanoff <jim@polarbeardesign.net>
2018-07-17 11:17:42 -0500

    added in passwords files

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,12 @@
+class PasswordsController < Devise::PasswordsController
+
+skip_before_filter :check_authorization, :check_authentication,
+  protected
+
+    # The path used after sending reset password instructions
+    def after_sending_reset_password_instructions_path_for(resource_name)
+      new_session_path(resource_name)
+      #password_reset_path
+    end
+
+end
